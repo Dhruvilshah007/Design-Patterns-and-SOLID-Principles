aaab566dfc811e6837c9ea6a548cae92bc455e36
Dhruvilshah007 <sdhruvil72@gmail.com>
2023-10-14 01:40:30 +0530

    Open-Closed Principle Done

---
+++ SOLID Principle/Open-Closed Principle/design_principles - OC - begin/src/main/java/com/company/openclose/PhoneSubscriber.java
@@ -0,0 +1,17 @@
+package com.company.openclose;
+
+import java.util.List;
+
+public class PhoneSubscriber extends Subscriber  {
+    
+    
+	
+	//Open for modification-open for extension
+    @Override
+    public double calculateBill() {
+        List<CallHistory.Call> sessions = CallHistory.getCurrentCalls(subscriberId);
+        long totalDuration = sessions.stream().mapToLong(CallHistory.Call::getDuration).sum();
+        return totalDuration*baseRate/100;
+    }
+
+}
